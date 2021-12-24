23a0433bf222f6a5f2808ab873e935ba2b5e037e
suvashkumar <suvashkumar.naogaon@gmail.com>
2021-12-24 22:52:04 +0600

    nothing

--- lesson 2/index.js
+++ lesson 2/index.js
@@ -4,18 +4,17 @@ const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
-
+/*
 const server = http.createServer((req, res) => {
     console.log('Request for ' + req.url + ' by method ' + req.method);
-  
     if (req.method == 'GET') {
       var fileUrl;
       if (req.url == '/') fileUrl = '/index.html';
       else fileUrl = req.url;
-  
-      var filePath = path.resolve('./public'+fileUrl);
+      var filePath = path.resolve("./public/"+fileUrl);
       const fileExt = path.extname(filePath);
-      if (fileExt == '.html') {
+      if(fileExt == '.html')
+      {
         fs.exists(filePath, (exists) => {
           if (!exists) {
             res.statusCode = 404;
@@ -43,6 +42,42 @@ const server = http.createServer((req, res) => {
                 ' not supported</h1></body></html>');
     }
   })
+  */
+
+
+const server = http.createServer((req, res) => {
+  if(req.method == 'GET'){
+    var fileurl;
+    if(req.url == '/') fileurl = '/index.html';
+    else fileurl = req.url;
+    var filePath = path.resolve('./public/'+fileurl);
+    const fileExt = path.extname(filePath);
+    if(fileExt == '.html')
+    {
+      fs.exists(filePath, (exists) => {
+        if(!exists)
+        {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end("<html><body>Error 404:"+req.method+" not supported</body></html>");
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+      })
+    }
+    else{
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end("<html><body>Error 999: "+fileurl+" is not a html file.</body></html>");
+    }
+  }
+  else{
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end("<html><body>Error 404:"+req.method+" not supported</body></html>");
+  }
+})
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
