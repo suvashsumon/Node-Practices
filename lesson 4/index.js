5d1de95953b29314172acdee03237f64cf2395e0
suvashkumar <suvashkumar.naogaon@gmail.com>
2022-01-06 23:10:04 +0600

    ejs template used

--- lesson 4/index.js
+++ lesson 4/index.js
@@ -22,6 +22,10 @@ app.get('/profile', (req, res)=>{
     res.render('profile', {user});
 })
 
+app.get('/login', (req, res)=>{
+    res.render('login');
+})
+
 app.get('*', (req, res)=>{
     res.sendFile(publicPath+"/404.html");
 })
